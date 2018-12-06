8ecdab4cf5c398bc48cf1e261205962917d249d5
Fábio Pegado <fabio-jordao@ufrn.edu.br>
2018-12-06 17:39:47 -0300

    Copiando arquivos para implementar um Service

---
+++ app/src/main/java/com/example/pegados/appdesafios/service/TimeService.java
@@ -0,0 +1,50 @@
+package com.example.pegados.exemploservicebind;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class TimeService extends Service {
+    private TimeWorker worker;
+
+    private TimeServiceBind binder = new TimeServiceBind();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        worker = new TimeWorker();
+        Log.i("MyApp", "Serviço Iniciado");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        new Thread(worker).start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        worker.stop();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+
+    }
+
+    public int getSeconds(){
+        return worker.getSeconds();
+    }
+
+    public class TimeServiceBind extends Binder{
+        public TimeService getService(){
+            return TimeService.this;
+        }
+    }
+}
